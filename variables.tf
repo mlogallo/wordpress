20d02fcc86e96ce5cdcd1c56adddf1189cce6cbd
akasshdeep9 <102315432+akasshdeep9@users.noreply.github.com>
2022-03-25 04:29:37 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+#__________________________________________________________
+#
+# Terraform Cloud Organization
+#__________________________________________________________
+
+variable "tfc_organization" {
+  default     = "Netnology-MINT"
+  description = "Terraform Cloud Organization."
+  type        = string
+}
+
+
+#______________________________________________
+#
+# Terraform Cloud kubeconfig Workspace
+#______________________________________________
+
+variable "ws_kubeconfig" {
+  default     = ""
+  description = "Intersight Kubernetes Service (IKS) kubeconfig Workspace Name.  The default value will be set to {cluster_name}_kubeconfig by the tfe variable module."
+  type        = string
+}
